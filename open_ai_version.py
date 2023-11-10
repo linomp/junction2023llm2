dffa5a3dd0159296fb926163d3810c075c335560
linomp <lm2p@hotmail.es>
2023-11-10 22:11:09 +0200

    add openai version

---
+++ open_ai_version.py
@@ -0,0 +1,19 @@
+import os
+
+from langchain.llms.openai import OpenAI
+
+os.environ["OPENAI_API_KEY"] = "..."
+os.environ["OPENAI_PROXY"] = "http://localhost:8080/v1/chat/completions"
+
+llm = OpenAI()
+llm("hello who are you")
+#
+# loader = TextLoader("./data/state_of_the_union.txt", encoding="utf-8")
+# documents = loader.load()
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# texts = text_splitter.split_documents(documents)
+#
+# embeddings = OpenAIEmbeddings()
+# docsearch = Chroma.from_documents(texts, embeddings)
+#
+# qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
