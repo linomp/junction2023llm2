bd49c22139fa668bab1ddd18cb1b1cc8a9e3e8e1
linomp <lm2p@hotmail.es>
2023-11-11 00:42:34 +0200

    update openai version

--- open_ai_version.py
+++ open_ai_version.py
@@ -1,19 +1,25 @@
-import os
-
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms.openai import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.chroma import Chroma
 
-os.environ["OPENAI_API_KEY"] = "..."
-# os.environ["OPENAI_PROXY"] = "http://localhost:8080"
+# os.environ["OPENAI_API_KEY"] = "KEY_DOESNT_MATTER_FOR_LOCALHOST"
+# os.environ["OPENAI_API_BASE"] = "http://localhost:8080"
 
-llm = OpenAI(openai_api_base="http://localhost:8080")
-llm("hello who are you")
-#
-# loader = TextLoader("./data/state_of_the_union.txt", encoding="utf-8")
-# documents = loader.load()
-# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# texts = text_splitter.split_documents(documents)
-#
-# embeddings = OpenAIEmbeddings()
-# docsearch = Chroma.from_documents(texts, embeddings)
-#
-# qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
+llm = OpenAI()
+
+loader = TextLoader("./data/state_of_the_union.txt", encoding="utf-8")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+docsearch = Chroma.from_documents(texts, embeddings)
+
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
+query = "What kept us apart last year?"
+res = qa.run(query)
+print(f"Q: {query}")
+print(f"A: {res}")
