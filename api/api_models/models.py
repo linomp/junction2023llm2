e212facaa2754c3300d164e4e80dd2a4f9788145
erkesh <erkesh@ttu.ee>
2023-11-11 16:26:57 +0200

    fix types

--- api/api_models/models.py
+++ api/api_models/models.py
@@ -2,18 +2,17 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-
-class Answer(BaseModel):
-    answer: str
-    confidence: float
-    sources: list[str]
-
-
-class Query(BaseModel):
-    value: str
-
-
 class InformationSource(BaseModel):
     url: Optional[str] = None
     title: str
     raw_content: Optional[str] = None
+
+class Answer(BaseModel):
+    answer: str
+    confidence: float
+    sources: list[InformationSource]
+
+class Query(BaseModel):
+    question: str
+
+
