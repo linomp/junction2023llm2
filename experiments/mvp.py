f343cba26da832fd25e73ba60b9515f605c94212
linomp <lm2p@hotmail.es>
2023-11-11 17:31:19 +0200

    add extra data

--- experiments/mvp.py
+++ experiments/mvp.py
@@ -13,14 +13,16 @@ from langchain.vectorstores.chroma import Chroma
 
 load_dotenv()
 
-loader = TextLoader("data/file1.txt", encoding="utf-8")
-documents = loader.load()
+sources = ["data/file1.txt", "data/file2.txt", "data/state_of_the_union_full.txt"]
+
+documents = []
+for source in sources:
+    documents.extend(TextLoader(source, encoding="utf-8").load())
+
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-documents.extend(TextLoader("data/file2.txt", encoding="utf-8").load())
-documents.extend(TextLoader("data/state_of_the_union_full.txt", encoding="utf-8").load())
 texts = text_splitter.split_documents(documents)
 
-llm = OpenAI(temperature= 0, model_name = 'text-davinci-003')
+llm = OpenAI(temperature=0, model_name='text-davinci-003')
 
 # TODO: serialize the embeddings and/or docsearch to disk, consider pickle
 embeddings = OpenAIEmbeddings()
@@ -39,7 +41,7 @@ PROMPT = PromptTemplate(
 
 chain_type_kwargs = {"prompt": PROMPT}
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever(),
-                                 chain_type_kwargs=chain_type_kwargs, return_source_documents = True)
+                                 chain_type_kwargs=chain_type_kwargs, return_source_documents=True)
 
 query = "Where does Olga live?"
 
@@ -47,10 +49,4 @@ result = qa({"query": query})
 
 print(f"Q: {query}")
 print(result['result'])
-print(list(map(lambda doc: doc.metadata,result['source_documents'])))
-
-
-
-
-
-
+print(list(map(lambda doc: doc.metadata, result['source_documents'])))
