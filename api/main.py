5ae82e738fe084b515908e4526040588685794ba
Steve389 <stevespositos@gmail.com>
2024-02-04 14:16:01 +0200

    change port number

--- api/main.py
+++ api/main.py
@@ -61,4 +61,4 @@ async def get_sources() -> list[InformationSource]:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
